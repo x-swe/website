074c076672be9076a4312948306fded14aef81c9
odi <odi@panter.ch>
2024-06-01 21:56:30 +0200

    add zod nodemailer and introduce mailService

---
+++ src/services/mailService.ts
@@ -0,0 +1,44 @@
+import nodemailer from "nodemailer";
+import { z } from "zod";
+
+const { NODEMAILER_PASSWORD, NODEMAILER_EMAIL } = z
+	.object({
+		NODEMAILER_PASSWORD: z.string(),
+		NODEMAILER_EMAIL: z.string(),
+	})
+	.parse(process.env);
+
+export type MailOptions = {
+	subject: string;
+	text: string;
+	replyTo: string;
+};
+
+export const sendMail = async (mailOptions: MailOptions) => {
+	const transporter = nodemailer.createTransport({
+		service: "gmail",
+		/* 
+            setting service as 'gmail' is same as providing these setings:
+
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true
+            
+            If you want to use a different email provider other than gmail, you need to provide these manually.
+            Or you can go use these well known services and their settings at
+            https://github.com/nodemailer/nodemailer/blob/master/lib/well-known/services.json
+        */
+		auth: {
+			user: NODEMAILER_EMAIL,
+			pass: NODEMAILER_PASSWORD,
+		},
+	});
+
+	return await new Promise<boolean>((resolve, reject) => {
+		// send mail
+		transporter.sendMail(
+			{ from: NODEMAILER_EMAIL, to: NODEMAILER_EMAIL, ...mailOptions },
+			(err) => (!err ? resolve(true) : reject(err)),
+		);
+	});
+};
