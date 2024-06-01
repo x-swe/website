1ee50c334e4e83c197ce3f4c688684a028525cd9
odi <odi@panter.ch>
2024-06-01 13:51:19 +0200

    Add home icon to navigation

--- src/components/navi.tsx
+++ src/components/navi.tsx
@@ -6,23 +6,43 @@ import { githubLink, xLink } from "@/app/config";
 import Link from "next/link";
 import dynamic from "next/dynamic";
 import { cn } from "@/lib/utils";
+import { Home } from "lucide-react";
 
 const Logo = dynamic(() => import("./logo"), { ssr: false });
+
+const linkClassName =
+	"transition-text duration-200 text-gray-500 dark:text-gray-400 hover:text-inherit dark:hover:text-inherit";
+const selectedLinkClassName = "select-none text-inherit dark:text-inherit";
 
 const Navi: FC = () => {
 	const pathname = usePathname();
 
 	return (
 		<header className="sticky top-0 z-50 bg-background opacity-90 flex items-center h-16 px-4 border-b shrink-0 md:px-6">
-			<Link
-				className="flex items-center gap-2 text-lg font-semibold sm:text-base mr-4"
-				href="/"
-			>
-				<Logo />
-			</Link>
 			<nav className="font-medium flex flex-row items-center gap-5 text-sm lg:gap-6">
+				<Logo />
 				<Link
-					className="transition-text duration-200 text-gray-500 dark:text-gray-400 hover:text-inherit dark:hover:text-inherit"
+					href="/"
+					title="Home"
+					className={cn(
+						linkClassName,
+						pathname === "/" && selectedLinkClassName,
+					)}
+				>
+					<Home className="w-4 h-4" />
+				</Link>
+				<Link
+					className={cn(
+						linkClassName,
+						pathname === "/contact" && selectedLinkClassName,
+					)}
+					href="/contact"
+					title="Contact"
+				>
+					Contact
+				</Link>
+				<Link
+					className={linkClassName}
 					href={xLink}
 					target="_blank"
 					rel="noopener noreferrer"
@@ -30,22 +50,12 @@ const Navi: FC = () => {
 					Community
 				</Link>
 				<Link
-					className="transition-text duration-200 text-gray-500 dark:text-gray-400 hover:text-inherit dark:hover:text-inherit"
+					className={linkClassName}
 					href={githubLink}
 					target="_blank"
 					rel="noopener noreferrer"
 				>
 					GitHub
-				</Link>
-				<Link
-					className={cn(
-						"transition-text duration-200 text-gray-500 dark:text-gray-400 hover:text-inherit dark:hover:text-inherit",
-						pathname === "/contact" &&
-							"font-bold select-none text-inherit dark:text-inherit",
-					)}
-					href="/contact"
-				>
-					Contact
 				</Link>
 			</nav>
 		</header>
