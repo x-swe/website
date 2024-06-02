b71bb761abd25ed996bbb71dc565b3c69b677228
odi <odi@panter.ch>
2024-06-02 20:03:09 +0200

    Update logo source based on resolved theme

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,9 +3,12 @@ import type { FC } from "react";
 import { useTheme } from "next-themes";
 
 const Logo: FC = () => {
-	const { theme } = useTheme();
+	const { resolvedTheme } = useTheme();
+
 	const logoSrc =
-		theme === "dark" ? `assets/logo_white.svg` : `assets/logo_black.svg`;
+		resolvedTheme === "dark"
+			? `assets/logo_white.svg`
+			: `assets/logo_black.svg`;
 	return (
 		<>
 			<img src={logoSrc} alt="logo" className="w-6 h-6 min-w-6 min-h-6" />
